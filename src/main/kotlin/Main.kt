a8cea9c54de4418e1e6c0318408c0a0d4ab84115
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-10-09 16:30:52 -0400

    Stage 3 of 6 for JetBrains Academy - Kotlin - Seam Carving project. • the program requirements are now to calculate the energy level of each pixel, and then using the highest energy level to normalize the intensity of each pixel and grey-scale the image ( where intensity = (255.0 * energy / maxEnergyValue).toInt). •  main() - adjusted to call energy() • energy() - loops through each pixel of the input image and calculates the energy level and saves it in a 2D array, and it checks the energy found for each pixel and sees if it is higher than the current max energy and if so, saves it in val maxEnergy. After getting all the energy levels, it then loops through each pixel again, and calculates the intensity and applies the number to the pixels red, green, and blue colors. Once it completes it returns the buffered image. • negativeImage() - was removed as it was no longer needed.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,14 @@ import java.awt.Color
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 var INPUT_IMAGE = ""
 var OUTPUT_IMAGE = ""
 
 fun main(args: Array<String>) {
-    if (commandsOkay(args)) saveImage(negativeImage())
+    if (commandsOkay(args)) saveImage(energy())
 }
 
 fun commandsOkay(args: Array<String>): Boolean {
@@ -38,14 +40,33 @@ fun commandsOkay(args: Array<String>): Boolean {
     return !(INPUT_IMAGE == "" || OUTPUT_IMAGE == "")
 }
 
-fun negativeImage(): BufferedImage {
+fun energy(): BufferedImage {
     val bImage = ImageIO.read(File(INPUT_IMAGE))
+    val energy = Array(bImage.width) { Array(bImage.height) { 0.0 } }
+    var maxEnergy = 0.0
 
     for (i in 0 until bImage.width) {
         for (j in 0 until bImage.height) {
-            var color = Color(bImage.getRGB(i, j))
+            val x = if (i == 0) 1 else if (i == bImage.width - 1) bImage.width - 2 else i
+            val y = if (j == 0) 1 else if (j == bImage.height - 1) bImage.height - 2 else j
+            val left = Color(bImage.getRGB(x - 1, j))
+            val right = Color(bImage.getRGB(x + 1, j))
+            val up = Color(bImage.getRGB(i, y - 1))
+            val down = Color(bImage.getRGB(i, y + 1))
+            val xDif = (left.red.toDouble() - right.red).pow(2) +
+                    (left.green.toDouble() - right.green).pow(2) + (left.blue.toDouble() - right.blue).pow(2)
+            val yDif = (up.red.toDouble() - down.red).pow(2) +
+                    (up.green.toDouble() - down.green).pow(2) + (up.blue.toDouble() - down.blue).pow(2)
+            val energyNum = sqrt(xDif + yDif)
 
-            color = Color(255 - color.red, 255 - color.green, 255 - color.blue)
+            if (energyNum > maxEnergy) maxEnergy = energyNum
+            energy[i][j] = energyNum
+        }
+    }
+    for (i in 0 until bImage.width) {
+        for (j in 0 until bImage.height) {
+            val intensity = (255.0 * energy[i][j] / maxEnergy).toInt()
+            val color = Color(intensity, intensity, intensity)
             bImage.setRGB(i, j, color.rgb)
         }
     }
